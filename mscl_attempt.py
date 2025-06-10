bb8a4d764a5f151fad5528f92e73ab60b8c6c189
KodingKale <kalebkwmouse@gmail.com>
2025-06-10 11:51:46 +0800

    wrong data type o Please enter the commit message for your changes. Lines starting

--- mscl_attempt.py
+++ mscl_attempt.py
@@ -82,7 +82,7 @@ def poll_data(log, imu):
             timestamp = packet.deviceTimestamp()
             points = packet.data()
             for dataPoint in points:
-                data_str = f"Time: {timestamp}, Data: {dataPoint.as_Vector()}"
+                data_str = f"Time: {timestamp}, Data: {dataPoint.as_string()}"
                 print(data_str)
                 log.write('\n' + data_str)
     except Exception as e:
