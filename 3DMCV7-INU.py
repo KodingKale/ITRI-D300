127664d9aaa4f66554ea106e4d63f2fdaaf487f6
KodingKale <kalebkwmouse@gmail.com>
2025-06-11 16:34:42 +0800

    PPS set and Polling is accurant proceeding to data streaming y Please enter the commit message for your changes. Lines starting

--- 3DMCV7-INU.py
+++ 3DMCV7-INU.py
@@ -21,12 +21,16 @@ def main():
     log = start_log()
     try:
         # Initialize IMU
-        imu = initialize_imu()
+        imu = initialize_imu(log)
         print("IMU initialized successfully")
+        log.write("IMU initialized succesfully\n")
         
         # Wait for sensor to stabilize
         time.sleep(0.5)
         
+        initialize_pps(imu, log)
+        print("PPS initialized succesfully")
+        log.write("PPS initialized succesfully\n")
         
 
         # Continuous reading loop
@@ -41,17 +45,17 @@ def main():
     except KeyboardInterrupt:
         print("\nExiting...")
         log.write("\nExiting...\n")
-    finally:
         imu.close()
+    finally:
         log.close()
         
-def initialize_imu():
+def initialize_imu(log):
     '''
     initialize UART port for 3DM-CV7-INS
     Default settings: 115200 baud, 8 data bits, 1 stop bit, no parity
     '''
     imu = serial.Serial(
-        port='/dev/ttyS0',
+        port='COM7',
         baudrate=115200,
         timeout=1
     )
@@ -65,14 +69,20 @@ def initialize_imu():
     checksum = fletcher_checksum(idle_command)
     imu.write(idle_command + checksum)
     time.sleep(0.1)
-    
-    # Enable data streaming
-    stream_command = bytes([0x75, 0x65, 0x01, 0x02, 0x02, 0x06])
-    checksum = fletcher_checksum(stream_command)
-    imu.write(stream_command + checksum)
-    time.sleep(0.1)
-    
+    ack = imu.read(10)
+    if ack != bytes([0x75, 0x65, 0x01, 0x04, 0x04, 0xF1, 0x02, 0x00, 0xD6, 0x6C]):
+        print("IMU Initialization Failed!")
+        log.write("IMU Initialization Failed!")
+        raise Exception("IMU Failed to Initialize")
     return imu
+
+def initialize_pps(imu, log):
+    imu.write(bytes([0x75, 0x65, 0x0C, 0x07, 0x07, 0x41, 0x01, 0x03, 0x02, 0x01, 0x00, 0x3C, 0x6D]))
+    ack = imu.read(10)
+    if ack != bytes([0x75, 0x65, 0x0C, 0x04, 0x04, 0xF1, 0x41, 0x00, 0x20, 0x2C]):
+        print("PPS Initialization Failed!")
+        log.write("IMU Initialization Failed!")
+        raise Exception("IMU Initialization Failed!")
 
 '''
 def parse_raw_data(raw_data, log):
@@ -87,46 +97,7 @@ def parse_raw_data(raw_data, log):
     # Parse the data
 '''
 
-def parse_acceleration_data(raw_data, log):
-    '''
-    Parse acceleration data from the sensor using struct.
-    MIP Packet Format:
-    - First 2 bytes: Header (0x75 0x65)
-    - Next bytes: Descriptor (0x80)
-    - Next byte: Payload length (0x0E)
-    Field:
-    - Next bytes: Field Length (0x0E)
-    - Next bytes: Field Descriptor (0x04)
-    - Next 12 bytes: acceleration data (3 floats, 4 bytes each for x, y, z)
-    Checksum:
-    - Last 2 bytes: Checksum
-    '''
-    try:
-        # Check if we have enough data
-        if len(raw_data) < 20:
-            print(f"Not enough data: {len(raw_data)} bytes")
-            return None
-            
-            
-        # Extract acceleration data (12 bytes, 3 floats)
-        # Using explicit little-endian format for float values
-        accel_data = raw_data[6:18]
-        accel_x, accel_y, accel_z = struct.unpack('>fff', accel_data)
-        
-        accel_x = accel_x * 9.80665
-        accel_y = accel_y * 9.80665
-        accel_z = accel_z * 9.80665
 
-        # Convert acceleration data to m/s^2
-        return {
-            'x': accel_x,
-            'y': accel_y,
-            'z': accel_z
-        }
-    except Exception as e:
-        print(f"Error parsing data: {e}")
-        log.write(f"Error parsing data: {e}\n")
-        return None
 
 def poll_imu_acceleration(imu, log):
     command = bytes([0x75, 0x65, 0x0C, 0x06, 0x06, 0x0D, 0x80, 0x01, 0x01, 0x04])
@@ -172,6 +143,37 @@ def read_imu_acceleration(imu, log):
         return None
     
     return parse_acceleration_data(raw_data, log)
+
+def parse_acceleration_data(raw_data, log):
+    '''
+    Parse acceleration data from the sensor using struct.
+    '''
+    try:
+        # Check if we have enough data
+        if len(raw_data) < 20:
+            print(f"Not enough data: {len(raw_data)} bytes")
+            return None
+            
+            
+        # Extract acceleration data (12 bytes, 3 floats)
+        # Using explicit little-endian format for float values
+        accel_data = raw_data[6:18]
+        accel_x, accel_y, accel_z = struct.unpack('>fff', accel_data)
+        
+        accel_x = accel_x * 9.80665
+        accel_y = accel_y * 9.80665
+        accel_z = accel_z * 9.80665
+
+        # Convert acceleration data to m/s^2
+        return {
+            'x': accel_x,
+            'y': accel_y,
+            'z': accel_z
+        }
+    except Exception as e:
+        print(f"Error parsing data: {e}")
+        log.write(f"Error parsing data: {e}\n")
+        return None
 
 def fletcher_checksum(data):
     '''
